f853916e8ae16484cfb005d540862072dca3c1f1
Liu-Yicheng <317176236@qq.com>
2018-09-15 15:48:12 +0800

    homework1

--- Python-homework/homework_one/homework_1.py
+++
@@ -1,6 +0,0 @@
-#1.os.lisdtir
-#2.with open(filename, mode) as f:
-#      f.readlines()  f.write()
-#3.str.split()   str.strip()
-#任务：将image文件夹下所有的图片名'-‘后面的数字写到一个1.txt文件夹下，一个图片名出来的数字写在一行
-#      读取1.txt文件，打印出里面每一行的内容
