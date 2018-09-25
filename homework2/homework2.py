364a3ce05e902f385cf6d944eac0b2eff27bc1b9
MINGBIN HOU <33325499+howBiGaStorm@users.noreply.github.com>
2018-09-25 12:42:25 +0800

    Update homework2.py

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -7,13 +7,13 @@ if not os.path.exists(result_dirpath):
     os.mkdir(result_dirpath)
 result_path = os.path.join(result_dirpath,'result.txt')
 
-g = (file for file in os.listdir(data_path))  # 文件名生成器
+file_generator = (file for file in os.listdir(data_path))  # 文件名生成器
 sum_list = []
 
 # 清空result.txt
-with open(result_path,'w') as f:
-    f.truncate()
-for i in g:
+with open(result_path,'w') as f1:
+    f1.truncate()
+for i in file_generator:
     value1 = int(i.strip().split('-')[1].split('.')[0])
     with open(os.path.join(data_path,i),'r') as f:
         value2 = int(f.readline())
